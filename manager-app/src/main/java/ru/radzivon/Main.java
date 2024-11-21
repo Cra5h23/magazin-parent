e12f54667f0bb444dfb212e257376924a80d9981
Nikolay <Novizdarialokin@yandex.ru>
2024-11-21 14:10:11 +0600

    feat: Заготовка Main

---
+++ manager-app/src/main/java/ru/radzivon/Main.java
@@ -0,0 +1,11 @@
+package ru.radzivon;
+
+/**
+ * @author Nikolai Radzivon
+ * @Date 21.11.2024
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
